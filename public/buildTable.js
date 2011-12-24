aaba3d5785c03c7484a3dbc10136fc6fcd41c66c
Parjanya Mudunuri <parjanya@gmail.com>
2011-12-24 06:00:36 +0000

    Using jade and express instead - Main index is now from views/index.jade

---
+++ public/buildTable.js
@@ -0,0 +1,50 @@
+var varSelectedCurrency = "GBP";
+var LINK_COUNTRIES = "images/flags/";
+var templateLinks = '<img src="' + LINK_COUNTRIES + '#{image}" onclick="setCurrency(\'#{ISO}\')"> | </img> ';
+var templateImageCurrency = '<img src="' + LINK_COUNTRIES + '#{image}")"></img> ';
+
+var currencies = {
+	"GBP" : {"ISO" : "GBP", "image": "gb.png", "pillars": {
+		"O/N" : "0.995", 
+		"1M" : "1.0",
+		"2M" : "1.2",
+		"3M" : "1.4"
+		}},
+	"INR" : {"ISO" : "INR", "image": "in.png", "pillars" : {
+	"O/N" : "0.993",
+	"S/N" : "0.997",
+	"T/N" : "0.999", 
+	"1M" : "1.0",
+	"2M" : "1.2",
+	"3M" : "1.4"
+	}}
+};
+
+function buildLinks() {
+	var links = "";
+	$.each(currencies, function() {
+		links += $.tmpl(templateLinks, this);
+	});
+	
+	$('#listOfLinks').html(links);
+}
+
+function buildTable() {
+	$('#divCurrency').html("Loading... Please Wait..");
+
+	var table = '<table border="1" id="tblCurrency">';
+	table += '<tr><th>' + $.tmpl(templateImageCurrency, currencies[varSelectedCurrency]) + '</th><th>Rate</th></tr>';
+
+	$.each(currencies[varSelectedCurrency].pillars, function(key, value) {
+		table += '<tr><td>' + key + '</td>' + '<td>' + value + '</td>' + '</tr>';
+	});
+
+	table += '</table>';
+
+	$('#divCurrency').html(table);	
+}
+
+function setCurrency(strCurrency) {
+	varSelectedCurrency = strCurrency;
+	buildTable();
+}
--- server.js
+++ server.js
@@ -1,16 +1,37 @@
 var http    = require('http'), 
     io      = require('socket.io'),
-    sys     = require('sys'),
-    express = require('express');
+    sys     = require('util'),
+    express = require('express'),
+    routes  = require('./routes');
 
 var port = 8111;
 
-var server = express.createServer();
-server.use(express.static(__dirname + '/public'));
-server.use(express.errorHandler({showStack: true, dumpExceptions: true}));
-server.listen(port);
+var app = module.exports = express.createServer();
 
-var socket = io.listen(server);
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', routes.index);
+app.listen(port);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+
+var socket = io.listen(app);
 
 socket.sockets.on('connection', function(client){
   var connected = true;
